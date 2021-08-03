c3bc0b0badeba1e6989f0c91dde056e4a294a501
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-08-03 23:23:57 +0100

    Images and Assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,23 @@
 import 'package:flutter/material.dart';
 
-int i = 0;
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('First App'),
-          centerTitle: true,
-          backgroundColor: Colors.deepOrange[200],
-        ),
-        body: Center(
-          child: Text('Hello Ninjas!',
-          style: TextStyle(
-            fontSize: 30.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.deepOrange[200],
-          child: Text("Click"),
-          onPressed: () {},
-        ),
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First App'),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrange[200],
       ),
-    ));
+      body: Center(child: Image.asset('assets/image3.jpg')),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepOrange[200],
+        child: Text("Click"),
+        onPressed: () {},
+      ),
+    );
+  }
+}
