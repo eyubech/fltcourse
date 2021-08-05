83210b828d035e41b39c6e8756682f24533d31f2
ayoubechchetyouy <ayoubechchetyouy19@gmail.com>
2021-08-05 20:10:46 +0100

    Custom Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,42 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
-void main() =>
-    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Quotelist(),
+    ));
 
-class Home extends StatelessWidget {
+class Quotelist extends StatefulWidget {
+  @override
+  _QuotelistState createState() => _QuotelistState();
+}
+
+class _QuotelistState extends State<Quotelist> {
+  List<Quote> quotes = [
+    Quote(
+        author: 'Oscar Wilde',
+        text: "Be yourself; everyone else is already taken."),
+    Quote(author: 'Frank Zappa', text: "So many books, so little time."),
+    Quote(
+        author: 'Marcus Tullius Cicero',
+        text: "A room without books is like a body without a soul."),
+  ];
+  List<String> authors = [
+    "Oscar Wilde",
+    "Frank Zappa",
+    "Marcus Tullius Cicero"
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('First App'),
+        title: Text("Awesome Quote"),
         centerTitle: true,
-        backgroundColor: Colors.deepOrange[200],
+        backgroundColor: Colors.redAccent,
       ),
-      body: Row(
-        children: <Widget>[
-          Expanded(child: Image.asset("assets/image1.jpg")),
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.redAccent,
-              child: Text("1"),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.blue,
-              child: Text("2"),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.purple,
-              child: Text("3"),
-            ),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.deepOrange[200],
-        child: Text("Click"),
-        onPressed: () {},
+      body: Column(
+        children: quotes.map((e) => Text("${e.text} - ${e.author}")).toList(),
       ),
     );
   }
